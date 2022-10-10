c233793c5616a87e09e7f2ab899ffebd69cefe64
Semyon <tonkih.sem@mail.ru>
2022-10-10 20:58:52 +0500

    Added DZ02

--- DZ02/Program.cs
+++ DZ02/Program.cs
@@ -4,3 +4,38 @@
 82 -> 10
 9012 -> 12
 */
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Ошибка ввода! Введите целое число: [{userLine}].");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+int sumOfDigits(int userNumber)
+{
+    int result = 0;
+    while (userNumber > 0)
+    {
+        result += userNumber % 10;
+        userNumber /= 10;
+    }
+    return result;
+}
+
+int userNumber = getNumberFromUser("Введите число: ");
+int result = sumOfDigits(userNumber);
+Console.WriteLine($"Сумма цифр в числе {userNumber} = {result}");
--- DZ03/Program.cs
+++ DZ03/Program.cs
@@ -4,3 +4,41 @@
 5 -> [1, 2, 5, 7, 19]
 3 -> [6, 1, 33]
 */
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Ошибка ввода! Введите целое число: [{userLine}].");
+        }
+    }
+    return result;
+}
+
+int userNumber = getNumberFromUser("Введите натуральное число от 1 до 99: ");
+int[] array = new int[userNumber];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+printArray(array);
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
