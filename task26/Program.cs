b976d99f12a9255061b7ce05d60c79e1e35626e0
Semyon <tonkih.sem@mail.ru>
2022-10-10 18:45:53 +0500

    Added task 26

---
+++ task26/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = (int)Math.Log10(userNumber) + 1;
+Console.WriteLine($"Количество знаков в числе {result}");
